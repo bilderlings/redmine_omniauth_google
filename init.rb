3a35da047e6854d21284eae4605139b8bcdaf09c
Dmitry Migunov <d.migunovs@bilderlings.com>
2023-07-06 17:28:49 +0300

    Upgrade deps

--- init.rb
+++ init.rb
@@ -1,18 +1,18 @@
-require 'redmine'
-require_dependency 'redmine_omniauth_google/hooks'
+require "redmine"
+require File.expand_path("lib/redmine_omniauth_google/hooks", __dir__)
 
 Redmine::Plugin.register :redmine_omniauth_google do
-  name 'Redmine Omniauth Google plugin'
-  author 'Dmitry Kovalenok'
-  description 'This is a plugin for Redmine registration through google'
-  version '0.0.1'
-  url 'https://github.com/twinslash/redmine_omniauth_google'
-  author_url 'http://twinslash.com'
+  name "Redmine Omniauth Google plugin"
+  author "Dmitry Kovalenok"
+  description "This is a plugin for Redmine registration through google"
+  version "0.0.1"
+  url "https://github.com/twinslash/redmine_omniauth_google"
+  author_url "http://twinslash.com"
 
   settings :default => {
     :client_id => "",
     :client_secret => "",
     :oauth_autentification => false,
-    :allowed_domains => ""
-  }, :partial => 'settings/google_settings'
+    :allowed_domains => "",
+  }, :partial => "settings/google_settings"
 end
